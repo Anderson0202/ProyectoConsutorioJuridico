64e8e00e12c86b6aa408b9aa84a9d377d845bdbe
Anderson <aorozom@unicesar.edu.co>
2021-09-24 08:51:30 -0500

    commit

---
+++ Consultorio/ClientApp/src/app/consultorio/consulta-caso/consulta-caso.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-consulta-caso',
+  templateUrl: './consulta-caso.component.html',
+  styleUrls: ['./consulta-caso.component.css']
+})
+export class ConsultaCasoComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
---
+++ Consultorio/ClientApp/src/app/consultorio/registro-caso/registro-caso.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-registro-caso',
+  templateUrl: './registro-caso.component.html',
+  styleUrls: ['./registro-caso.component.css']
+})
+export class RegistroCasoComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
