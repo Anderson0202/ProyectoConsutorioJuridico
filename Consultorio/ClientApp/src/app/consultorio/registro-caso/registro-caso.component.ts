495b1b8919a41883fa53968e562cad3557837d16
hacadena28 <hacadena@unicesar.edu.cong>
2022-06-29 16:02:42 -0500

    commit 3

--- Consultorio/ClientApp/src/app/consultorio/registro-caso/registro-caso.component.ts
+++ Consultorio/ClientApp/src/app/consultorio/registro-caso/registro-caso.component.ts
@@ -1,3 +1,4 @@
+import { NgIf } from '@angular/common';
 import { Component, OnInit } from '@angular/core';
 import { UsuarioService } from 'src/app/services/usuario.service';
 import { IndexedAccessType } from 'typescript';
@@ -16,11 +17,33 @@ export class RegistroCasoComponent implements OnInit {
    }
    personas:Persona[];
    persona:Persona;
+   p: Persona;
    item:number;
   ngOnInit(): void {
     this.usuarioService.getAllPersonas().subscribe(result => {
             this.personas = result;
   })
+  }
+  
+
+
+  getPersona(persona:Persona|number): any {
+
+    this.usuarioService.getPersonas(persona).subscribe(result => {
+      this.p = result;
+      console.log(this.p);
+    })
+    
+  }
+  deletePersonas(persona: Persona|number): void {
+    this.usuarioService.deletePersonas(persona).subscribe(result => {
+      this.persona = result;
+      console.log('eliminado')
+      window.location.reload();
+      
+    })
+
+    
   }
 
   
